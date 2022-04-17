ab8862c34621dd070dffc18900166f36d6540bcf
Kiril Zyapkov <ziapkov@gmail.com>
2022-04-17 17:19:05 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKjDJasu-DRFX7ZPT4_mgkTDUnERqQ2EM",
+  authDomain: "my-storage-3c7bc.firebaseapp.com",
+  projectId: "my-storage-3c7bc",
+  storageBucket: "my-storage-3c7bc.appspot.com",
+  messagingSenderId: "716964169771",
+  appId: "1:716964169771:web:51ac090f5a03226013426e"
+};
+
+
+const app = initializeApp(firebaseConfig);
