9ab0541c1f9f8acf070d764a963233b95c0329bb
Kiril Zyapkov <ziapkov@gmail.com>
2022-04-17 20:20:15 +0300

    Creating register form in Register component.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Register() {
-  return (
-    <div>Register</div>
-  )
+    return (
+        <div className="container-register">
+            <form>
+                <div className="wrapper-register">
+                    <h1>Register</h1>
+                    <p>Please fill in this form to create an account.</p>
+                    <hr />
+
+                    <label for="email"><b>Email</b></label>
+                    <input type="text" placeholder="Enter Email" name="email" id="email" required />
+
+                    <label for="password"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="password" id="password" required />
+
+                    <label for="rePass"><b>Repeat Password</b></label>
+                    <input type="password" placeholder="Repeat Password" name="rePass" id="rePass" required />
+                    <hr />
+
+                    <button type="submit" className="registerbtn">Register</button>
+                </div>
+
+                <div className="container signin">
+                    <p>Already have an account? <Link to={'/login'}>Sign in</Link>.</p>
+                </div>
+            </form>
+        </div>
+    )
 }
 
-export default Register
+export default Register;
